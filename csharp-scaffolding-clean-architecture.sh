ecde9434015624b40fc08d5d4f6c91a685f0e014
rslewenstein <rafasoaresjf@gmail.com>
2023-10-07 12:28:26 -0300

    FEAT: creating initials functions

--- csharp-scaffolding-clean-architecture.sh
+++ csharp-scaffolding-clean-architecture.sh
@@ -6,4 +6,25 @@
 # https://github.com/rslewenstein
 ################################
 
+read -p "Type the project name: " projectName
+read -p "Do you want enable cors? (0-No, 1-yes) " enableCors
 
+createMainFolder(){
+    mkdir $projectName
+    cd $projectName
+}
+
+createOtherFiles(){
+    touch README.md
+    echo "# $projectName" >> README.md
+
+    touch .gitignore
+    cat > '.gitignore' << EOT
+    .vscode
+    csharp-scaffolding-clean-architecture.sh
+EOT
+}
+
+
+createMainFolder
+createOtherFiles
